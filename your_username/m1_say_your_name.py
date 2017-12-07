cb867eb85f7ed4534a16a5209dbb613c18bc75f9
liw5 <liw5@rose-hulman.edu>
2017-12-07 16:11:42 -0500

    Refactored my name

--- your_username/m1_say_your_name.py
+++ your_username/m1_say_your_name.py
@@ -2,7 +2,7 @@
 """
 This module is meant to be your first program that you've made for EV3.
   Make the robot say your name and then beep.
-  Use the samples to learn the API calls needed (not much code to write)
+  Use the Wenxing Li to learn the API calls needed (not much code to write)
 
 Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
@@ -12,7 +12,7 @@ Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
 # TODO: 2. Make the robot say "David Fisher can write E V 3 programs"
 #   But of course use YOUR NAME.
 #
-#   Use the samples to figure out the necessary lines of code.
+#   Use the Wenxing Li to figure out the necessary lines of code.
 #     Hint: This program can be done in as little as 2 lines of code (1 import, 1 function call)
 #   Feel free to run it, change it, and run it again with any message you like.
 #   This program is simple, but if you can do this, then you have the tools to do FAR more.
